b83aabc019b8712e6e4c77587ef7742c09246fa0
散里散糕 <64390704+Lorry-San@users.noreply.github.com>
2025-10-25 20:13:57 +0800

    Update xrayinstall.sh

--- xrayinstall.sh
+++ xrayinstall.sh
@@ -387,7 +387,7 @@ get_latest_version() {
       echo "error: github API rate limit exceeded"
     else
       echo "error: Failed to get the latest release version."
-      echo "Welcome bug report:https://gh.85613540.xyz/https://github.com/XTLS/Xray-install/issues"
+      echo "Welcome bug report:https://github.com/XTLS/Xray-install/issues"
     fi
     "rm" "$tmp_file"
     exit 1
@@ -417,7 +417,7 @@ get_latest_version() {
   local i url_zip
   for i in "${!releases_list[@]}"; do
     releases_list["$i"]="v${releases_list[$i]#v}"
-    url_zip="https://gh.85613540.xyz/https://github.com/XTLS/Xray-core/releases/download/${releases_list[$i]}/Xray-linux-$MACHINE.zip"
+    url_zip="https://github.com/XTLS/Xray-core/releases/download/${releases_list[$i]}/Xray-linux-$MACHINE.zip"
     if grep -q "$url_zip" "$tmp_file"; then
       PRE_RELEASE_LATEST="${releases_list[$i]}"
       break
@@ -431,7 +431,7 @@ version_gt() {
 }
 
 download_xray() {
-  local DOWNLOAD_LINK="https://gh.85613540.xyz/https://github.com/XTLS/Xray-core/releases/download/${INSTALL_VERSION}/Xray-linux-${MACHINE}.zip"
+  local DOWNLOAD_LINK="https://github.com/XTLS/Xray-core/releases/download/${INSTALL_VERSION}/Xray-linux-${MACHINE}.zip"
   echo "Downloading Xray archive: $DOWNLOAD_LINK"
   if curl -f -x "${PROXY}" -R -H 'Cache-Control: no-cache' -o "$ZIP_FILE" "$DOWNLOAD_LINK"; then
     echo "ok."
