8a7a672868fbc677b15f2ffc11bfb51d3d2b5bb7
散里散糕 <64390704+Lorry-San@users.noreply.github.com>
2025-10-25 20:14:19 +0800

    Update xrayinstall-alpine.sh

--- xrayinstall-alpine.sh
+++ xrayinstall-alpine.sh
@@ -2,7 +2,7 @@
 set -e
 apk add --no-cache curl unzip
 XRAY_VERSION=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep tag_name | cut -d '"' -f 4)
-curl -L -o /tmp/xray.zip https://gh.85613540.xyz/https://github.com/XTLS/Xray-core/releases/download/${XRAY_VERSION}/Xray-linux-64.zip
+curl -L -o /tmp/xray.zip https://github.com/XTLS/Xray-core/releases/download/${XRAY_VERSION}/Xray-linux-64.zip
 unzip /tmp/xray.zip -d /tmp/xray
 mv /tmp/xray/xray /usr/local/bin/
 chmod +x /usr/local/bin/xray
